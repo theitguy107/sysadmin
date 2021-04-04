b7803737d055133d923ca06fa01a8663aa182b8a
Joshua Morden <45412544+theitguy107@users.noreply.github.com>
2021-04-04 13:47:19 -0400

    Add files via upload

---
+++ Start_Veeam_services.ps1
@@ -0,0 +1,3 @@
+﻿get-service 'MSSQL$VEEAMSQL2016' | Where {$_.Status -eq 'Stopped'} | start-service
+get-service 'SQLTELEMETRY$VEEAMSQL2016' | Where {$_.Status -eq 'Stopped'} | start-service
+get-service -name veeam* | Where {$_.Status -eq 'Stopped'} | start-service
